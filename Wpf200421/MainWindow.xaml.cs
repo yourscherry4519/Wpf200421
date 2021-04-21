6f87eb8ccb9a97f469ad1469cc6d16888a580710
yourscherry4519@gmail.com <yourscherry4519@gmail.com>
2021-04-21 13:56:20 +0500

    Добавьте файлы проекта.

---
+++ Wpf200421/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf200421
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void ButtonOpen_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpen.Visibility = Visibility.Collapsed;
+            ButtonClose.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Page pg = null;
+            switch(((ListViewItem)((ListView)sender).SelectedItem).Name)
+            {
+                case "MainItem":
+                    pg = new Page1();
+                    MainFrame.Navigate(pg);
+                    break;
+
+                case "FirstItem":
+                    pg = new Page2();
+                    MainFrame.Navigate(pg);
+                    break;
+
+                case "ForItem":
+                    pg = new Page3();
+                    MainFrame.Navigate(pg);
+                    break;
+
+                case "SItem":
+                    pg = new Page4();
+                    MainFrame.Navigate(pg);
+                    break;
+
+                case "DItem":
+                    pg = new Page5();
+                    MainFrame.Navigate(pg);
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Wpf200421/Page5.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf200421
+{
+    /// <summary>
+    /// Логика взаимодействия для Page5.xaml
+    /// </summary>
+    public partial class Page5 : Page
+    {
+        public Page5()
+        {
+            InitializeComponent();
+        }
+    }
+}
